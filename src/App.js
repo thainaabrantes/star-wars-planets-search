fcf50c6a88017f473337ad0c8fcb0e2330fb8106
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2023-01-23 23:12:53 -0300

    Faz uma requisição p/ a API e cria a tabela com os dados retornados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Table from './components/Table';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => {
+        const planetsWithoutResidents = data.results.map((result) => {
+          delete result.residents;
+          return result;
+        });
+        setPlanets(planetsWithoutResidents);
+      });
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <div><Table planets={ planets } /></div>
   );
 }
 
